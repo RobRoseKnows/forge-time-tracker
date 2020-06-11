9b5fa7fbcf396a69632fa4bbf24580ab4f247eec
Rob Rose <RobRoseKnows+Git@gmail.com>
2020-06-11 15:15:36 -0400

    Added set and get user start times.

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,30 @@
-import ForgeUI, { render, Text, IssuePanel, Fragment, useState } from '@forge/ui';
+import api from '@forge/api';
+import ForgeUI, { render, Text, IssuePanel, Fragment, useProductContext, useState } from '@forge/ui';
+
+const STORAGE_NAMESPACE = "io.robrose.timetracker"
+
+const now = () => {
+  return new Date().getTime();
+}
+
+const setUserStartTime = async (issueId, userId) => {
+  const setRes = api.store
+    .onJiraIssue(issueId)
+    .set(`${STORAGE_NAMESPACE}/${userId}/start`, now());
+    
+  console.info(`Start time setten for ${userId} on ${issueId}`);
+  return setRes;
+}
+
+const fetchUserStartTime = async (issueId, userId) => {
+  const getRes = await api.store
+    .onJiraIssue(issueId)
+    .get(`${STORAGE_NAMESPACE}/${userId}/start`);
+  
+  console.info(`Start time gotten for ${userId} on ${issueId}`);
+  return getRes;
+};
+
 
 const App = () => {
   return (
